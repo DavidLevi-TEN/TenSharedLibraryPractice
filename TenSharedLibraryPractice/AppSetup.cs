de3efb0428a06a3c83f1778d5d2478acf19e7209
DavidLevi-TEN <DavidLevi@tengroup.com>
2024-04-30 09:20:18 +0100

    Add project files.

---
+++ TenSharedLibraryPractice/AppSetup.cs
@@ -0,0 +1,26 @@
+﻿using Ten.Shared.AI.Application.EvilAiStack.Extensions;
+using Ten.Shared.AI.Application.EvilAiStack.Secrets;
+using Ten.Shared.Health;
+using Ten.Shared.Hosting.AspNetCore.Builder;
+using Ten.Shared.Secrets;
+
+namespace TenSharedLibraryPractice
+{
+    public static class AppSetup
+    {
+        public static void Configure(TenWebAppBuilder builder)
+        {
+            builder.AddTenHealthChecks();
+
+            var services = builder.Services;
+            var configuration = builder.Configuration;
+
+            services.AddSecretsVault<EvilAiAppSecretsCache>(configuration);
+            services.AddEvilAiAppServices(configuration);
+
+            services.AddLogging();
+
+            services.AddControllers();
+        }
+    }
+}
---
+++ TenSharedLibraryPractice/Program.cs
@@ -0,0 +1,13 @@
+using TenSharedLibraryPractice;
+using Ten.Shared.Templates;
+
+var options = new AspNetCoreOptions("TenSharedLibraryPractice", "TSLP", args)
+{
+    ConfigureBuilder = AppSetup.Configure
+};
+
+var app = AspNetCoreTemplate.Create(options);
+
+app.MapControllers();
+
+app.Run();
